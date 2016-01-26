cdd9ee1b19a059927480d1560359cfbc29dd3d3a
Giulio Maria Facin <giulio.maria@gmail.com>
2016-01-26 12:59:23 +0100

    aggiunta caloreplus

--- app.js
+++ app.js
@@ -15,35 +15,56 @@ var tmp="<li>" +
 var Controller = {
     init: function(){
         View.init();
-        View.show(data);
+        
     },
-    min: function(giorno){
+    min: function(settimana, giorno){
         var minima=100;
-        for(var i=0;i<data.length;i++){
-            if(data[i].day==giorno){
-                if(data[i].temperature<minima){
-                    minima=data[i].temperature;
+        for(var i=0;i<settimana.length;i++){
+            if(settimana[i].day==giorno){
+                if(settimana[i].temperature<minima){
+                    minima=settimana[i].temperature;
                 }
             }
         }
         return minima;
     },
-    max: function(giorno){
+    max: function(settimana, giorno){
          var massima=-100;
-        for(var i=0;i<data.length;i++){
-            if(data[i].day==giorno){
-                if(data[i].temperature>massima){
-                    massima=data[i].temperature;
+        for(var i=0;i<settimana.length;i++){
+            if(settimana[i].day==giorno){
+                if(settimana[i].temperature>massima){
+                    massima=settimana[i].temperature;
                 }
             }
         }
         return massima;
+    },
+    getData: function(){
+        return data;
     }
 }
 
 var View = {
     init: function(){
-        
+        View.startfilter();
+    },
+    startfilter: function(){
+        $("#btn-filter").on("click", function (event) {
+            var citta= $("#sel").val();
+            var arraydata = Controller.getData();
+            var arraycitta = [];
+            var j=0;
+            for(var i=0; i<arraydata.length; i++){
+                
+                if(arraydata[i].city==citta){
+                    console.log("ciao");
+                    arraycitta[j]=arraydata[i];
+                    j++;
+                }
+            }
+            View.show(arraycitta);
+        }
+      )
     },
     show: function(Day){
         $("#summary").html("");
@@ -51,8 +72,8 @@ var View = {
             if(((i+1)%4)==0){
             var item = tmp.replace("I", Day[i].condition);
             item= item.replace("D", Day[i].day);
-            item= item.replace("X", Controller.min(Day[i].day));
-            item= item.replace("Y", Controller.max(Day[i].day));
+            item= item.replace("X", Controller.min(Day, Day[i].day));
+            item= item.replace("Y", Controller.max(Day, Day[i].day));
             $("#summary").append(item);
             }
         }
